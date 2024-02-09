3a387857d132188bdf990012cb9168915f1944bd
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-02-09 15:01:23 +0200

    Update bufferedNetworkRequest.js

--- bufferedNetworkRequest.js
+++ bufferedNetworkRequest.js
@@ -1,2 +1,46 @@
 
-
+async function bufferedNetworkRequest(request, updateCallback) {
+  
+  const response = await request;
+  
+  let respText = '';
+  
+  const res = new Response(new ReadableStream({
+    
+    async start(controller) {
+      
+      const reader = response.body.getReader();
+      
+      for (;;) {
+        
+        const {done, value} = await reader.read();
+        
+        if (!done) {
+          
+          const string = new TextDecoder().decode(value);
+          
+          respText += string;
+          
+          
+          const validData = InvalidJSONParser.parse(respText);
+          
+          updateCallback(validData);
+          
+        } else {
+          
+          updateCallback(null, done);
+          
+          break;
+          
+        }
+        
+      }
+      
+      controller.close();
+      
+    }
+    
+  }));
+
+}
+
