ca7455c27cea0c5e9935931357d2d9c6f7fd15c6
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-02-26 21:04:23 +0200

    Update demo.js

--- demos/demo.js
+++ demos/demo.js
@@ -1,2 +1,43 @@
 
-
+const statusEl = document.querySelector('.status');
+
+async function main() {
+  
+  statusEl.classList.add('loading');
+  
+  
+  let request = fetch('https://api.github.com/user/repos?visibility=all&sort=pushed&per_page=100&page=1', {
+    headers: {
+      authorization: 'token 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'
+    },
+    cache: 'no-store',
+    method: 'GET'
+  });
+  
+  
+  request = await BufferedNetworkRequest(request, { json: true });
+    
+  request.onupdate = (data) => {
+    
+    let out = '';
+  
+    data.forEach(item => {
+  
+      out += '<p>' + item.name + '</p>';
+      
+    });
+    
+    statusEl.innerHTML += out;
+    
+  };
+  
+  request.ondone = () => {
+    
+    statusEl.classList.remove('loading');
+    
+  };
+  
+}
+
+main();
+
